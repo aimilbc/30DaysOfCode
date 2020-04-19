59b96594497389bc64d89648b011c35c0cfe1acc
Aimi Ross <touyamaaimi922@gmail.com>
2020-04-18 21:45:59 -0700

    Day 17
    More Exceptions

---
+++ 17_Java_More_Exceptions/More_Exceptions.java
@@ -0,0 +1,38 @@
+mport java.io.*;
+
+class Calculator{
+    //default constructor
+    Calculator(){}
+    public int power(int N, int P)throws Exception{
+        // error base: throw exception
+        if(N<0 || P<0){
+            throw new Exception("n and p should be non-negative");
+        }else{
+            // execute the problem.
+            return (int) Math.pow(N, P);
+        }
+    }
+}
+
+class Solution{
+
+    public static void main(String[] args) {
+    
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t-- > 0) {
+        
+            int n = in.nextInt();
+            int p = in.nextInt();
+            Calculator myCalculator = new Calculator();
+            try {
+                int ans = myCalculator.power(n, p);
+                System.out.println(ans);
+            }
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        in.close();
+    }
+}
