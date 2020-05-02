7d6bfb337b45d8bc76362a75a59f231d88b32ed4
aimilbc <55858907+aimilbc@users.noreply.github.com>
2020-05-02 10:38:39 -0700

    Day 25
    Big O notation

---
+++ 25_Java_Big_O_Notation/findPrime.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static boolean check(int num){
+        for (int i = 2; i <= Math.sqrt(num); i++){
+            if(num % i == 0)
+                return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int count = in.nextInt();
+
+        for (int i = 0; i < count ; i++){
+            int num = in.nextInt();
+            if(num > 1 && check(num)){
+                System.out.println("Prime");
+            }else
+                System.out.println("Not prime");
+        } 
+    }
+}
